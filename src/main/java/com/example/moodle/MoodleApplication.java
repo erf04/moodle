acef499762fee2566367a065deb6c262e1cc5116
AmirrezaSedghamiz <amir7007.sed@gmail.com>
2024-01-28 18:53:56 +0330

    dataSaver

--- src/main/java/com/example/moodle/MoodleApplication.java
+++ src/main/java/com/example/moodle/MoodleApplication.java
@@ -1,11 +1,22 @@
 package com.example.moodle;
 
+import com.example.moodle.model.Course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import java.util.Arrays;
+
 @SpringBootApplication(exclude = {SecurityAutoConfiguration.class })
 public class MoodleApplication {
+
+	static Course[] courses = new Course[0];
+	public static void dataSaver(Course course)
+	{
+		Arrays.copyOf(courses,courses.length+1);
+		courses[courses.length-1] = course;
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MoodleApplication.class, args);
