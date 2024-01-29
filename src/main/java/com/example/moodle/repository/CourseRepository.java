c36c453a319bd012b5b3138b3cfa505db8e8d66f
erfan <erfank20041382@gmail.com>
2024-01-29 18:39:15 +0330

    exam html

--- src/main/java/com/example/moodle/repository/CourseRepository.java
+++ src/main/java/com/example/moodle/repository/CourseRepository.java
@@ -1,17 +1,12 @@
 package com.example.moodle.repository;
 
-<<<<<<< HEAD
+
 import com.example.moodle.model.Account;
-=======
->>>>>>> 4c6f7e584df2acb1026d8d56106c49b476943ee2
 import com.example.moodle.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
-<<<<<<< HEAD
 
-=======
->>>>>>> 4c6f7e584df2acb1026d8d56106c49b476943ee2
 }
