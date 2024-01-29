b4686c1c7e6e5f23439e64739208460b7d09a714
AramAsadinejad <jeonaram57@gmail.com>
2024-01-30 01:59:58 +0330

    .

--- src/main/java/com/example/moodle/controller/CourseController.java
+++ src/main/java/com/example/moodle/controller/CourseController.java
@@ -1,18 +1,22 @@
 package com.example.moodle.controller;
 
-import com.example.moodle.model.*;
-import com.example.moodle.repository.CoursePlanRepository;
+import com.example.moodle.model.Account;
+import com.example.moodle.model.Admin;
+import com.example.moodle.model.Course;
+import com.example.moodle.model.CoursePlan;
 import com.example.moodle.repository.CourseRepository;
 import com.example.moodle.service.AccountService;
+import com.example.moodle.service.CoursePlanService;
+
+import com.example.moodle.service.TeacherService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Date;
+import java.util.List;
 
 
 @Controller
@@ -22,47 +26,56 @@ public class CourseController {
     @Autowired
     private CourseRepository courseRepository;
     @Autowired
-    private CoursePlanRepository coursePlanRepository;
+    private CoursePlanService coursePlanService;
+
+    @Autowired
+    private TeacherService teacherService;
     @GetMapping("/addcourse/{user_id}")
     public String showAddCourse(@PathVariable Long user_id, Model model) {
         Account user=accountService.findByID(user_id);
+        model.addAttribute("user",user);
         if(user instanceof Admin){
             Course course=new Course();
             model.addAttribute("course",course);
-            model.addAttribute("user",accountService.findByID(user_id));
+
             return "addCourse";
         }
-        else return null;
+        else{
+            return "error500";
+        }
+
 
     }
     @PostMapping("/saveCourse/{user_id}")
     public  String courseSave(@ModelAttribute("course") Course course,@PathVariable("user_id") Long id){
         System.out.println(course.getName());
-        courseRepository.save(course);
-        return "redirect:/addcourse/"+id;
-
+        Account teacher= teacherService.findTeacherById(id);
+        if (teacher!=null){
+            courseRepository.save(course);
+            return "redirect:/addcourse/"+id;
+        }
+        else{
+            return "error500";
+        }
     }
-    @GetMapping("/addcourseplan/{user_id}")
-    public String showAddCoursePlan(@PathVariable("user_id") long user_id,Model model){
-        Account user=accountService.findByID(user_id);
 
-            CoursePlan coursePlan=new CoursePlan();
-            coursePlan.setParticipants(new ArrayList<>());
-            coursePlan.setCreator(user);
-            coursePlan.setCreationTime(LocalDateTime.now());
-            coursePlan.setStartTime(LocalDateTime.now());
-            coursePlan.setEndTime(LocalDateTime.now());
-            coursePlan.setExams(new ArrayList<>());
-            model.addAttribute("coursePlan",coursePlan);
-            model.addAttribute("user",accountService.findByID(user_id));
-            model.addAttribute("courses",courseRepository.findAll());
-            return "addCoursePlan";
 
-    }
-    @PostMapping("/savecourseplan/{user_id}")
-    public String saveCoursePlan(@ModelAttribute("course") CoursePlan coursePlan,@PathVariable("user_id") Long id) {
-        System.out.println(coursePlan.getName());
-        coursePlanRepository.save(coursePlan);
-        return "redirect:/addcourseplan/" + id;
+
+
+    @PostMapping("/searchCourse/{user_id}")
+    public  String courseSearch(Model model,@RequestParam("searchedContent") String partialCourseName,@PathVariable("user_id") Long id){
+        Account account=accountService.findByID(id);
+        List<CoursePlan> coursePlans=accountService.findCoursePlansByAccountId(id);
+        model.addAttribute("user",account);
+        model.addAttribute("courseplans",coursePlans);
+        System.out.println(partialCourseName);
+        List<Course> courses=courseRepository.findAllByNameContaining(partialCourseName);
+        List<CoursePlan> coursePlansNew=new ArrayList<>();
+        for (Course course:courses){
+            coursePlansNew.addAll(coursePlanService.findByCourse(course));
+        }
+
+        System.out.println(coursePlans.get(0));
+        return "searched";
     }
 }
--- src/main/java/com/example/moodle/repository/CourseRepository.java
+++ src/main/java/com/example/moodle/repository/CourseRepository.java
@@ -1,12 +1,13 @@
 package com.example.moodle.repository;
 
-import com.example.moodle.model.Account;
-
 import com.example.moodle.model.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course,Long> {
+    List<Course> findAllByNameContaining(String name);
 
 }
